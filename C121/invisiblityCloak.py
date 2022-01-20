cf67c335f84f6ac7d97e273d62fa04964fc8f667
HussainMadarwala53 <70691828+HussainMadarwala53@users.noreply.github.com>
2022-01-20 18:19:25 +0530

    Update invisiblityCloak.py

--- C121/invisiblityCloak.py
+++ C121/invisiblityCloak.py
@@ -1,70 +1,56 @@
 import cv2
-import time
 import numpy as np
+import time
 
-#To save the output in a file output.avi
+#to save the output in avi format
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-output_file = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
-
-#Starting the webcam
-cap = cv2.VideoCapture(0)
-
-#Allowing the webcam to start by making the code sleep for 2 seconds
+output_file = cv2.VideoWriter('output.avi', fourcc, 20.0,(640,48))
+#to start the camera
+cam = cv2.VideoCapture(0)
+#to start the program after two seconds
 time.sleep(2)
 bg = 0
-
-#Capturing background for 60 frames
+#to capture the background for 60 frames
 for i in range(60):
-    ret, bg = cap.read()
-#Flipping the background
-bg = np.flip(bg, axis=1)
+    #to return the read images inside the bg var
+    ret, bg = cam.read()
+#to flip the bg img
+bg = np.flip(bg,axis = 1)
 
-#Reading the captured frame until the camera is open
-while (cap.isOpened()):
-    ret, img = cap.read()
+#to read the captured images until the camera is open
+while(cam.isOpened()):
+    ret, img = cam.read()
     if not ret:
         break
-    #Flipping the image for consistency
-    img = np.flip(img, axis=1)
-
-    #Converting the color from BGR to HSV
+    img = np.flip(img,axis = 1)
+    #to convert the color of the img from bgr to hsv
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
-    #Generating mask to detect red colour
-    #These values can also be changed as per the color
-    lower_red = np.array([0, 120, 50])
-    upper_red = np.array([10, 255,255])
-    mask_1 = cv2.inRange(hsv, lower_red, upper_red)
-
-    lower_red = np.array([170, 120, 70])
-    upper_red = np.array([180, 255, 255])
-    mask_2 = cv2.inRange(hsv, lower_red, upper_red)
-
-    mask_1 = mask_1 + mask_2
-
-    #Open and expand the image where there is mask 1 (color)
-    mask_1 = cv2.morphologyEx(mask_1, cv2.MORPH_OPEN, np.ones((3, 3), np.uint8))
-    mask_1 = cv2.morphologyEx(mask_1, cv2.MORPH_DILATE, np.ones((3, 3), np.uint8))
-
-    #Selecting only the part that does not have mask one and saving in mask 2
-    mask_2 = cv2.bitwise_not(mask_1)
-
-    #Keeping only the part of the images without the red color 
-    #(or any other color you may choose)
-    res_1 = cv2.bitwise_and(img, img, mask=mask_2)
-
-    #Keeping only the part of the images with the red color
-    #(or any other color you may choose)
-    res_2 = cv2.bitwise_and(bg, bg, mask=mask_1)
-
-    #Generating the final output by merging res_1 and res_2
-    final_output = cv2.addWeighted(res_1, 1, res_2, 1, 0)
+    #to generate a mask to detect red color
+    lower_red = np.array([0,120,50])
+    upper_red = np.array([10,255,255])
+    mask_one = cv2.inRange(hsv,lower_red,upper_red)
+    
+    lower_red = np.array([170,120,70])
+    upper_red = np.array([180,255,255])
+    mask_two = cv2.inRange(hsv,lower_red,upper_red)
+    
+    mask_one=mask_one+mask_two
+    #to open and expand the img where there is mask_one color
+    mask_one = cv2.morphologyEx(mask_one, cv2.MORPH_OPEN, np.ones((3,3), np.uint8))
+    mask_one = cv2.morphologyEx(mask_one, cv2.MORPH_DILATE, np.ones((3,3), np.uint8))
+    
+    #to select the part tat doesnot have mask one
+    mask_two = cv2.bitwise_not(mask_one)
+    
+    #to save the part of the img without using red color
+    res_one = cv2.bitwise_and(img,img,mask=mask_two)
+    res_two = cv2.bitwise_and(bg,bg,mask=mask_two)
+    #to generate the final output
+    final_output = cv2.addWeighted(res_one, 1, res_two, 1,0)
     output_file.write(final_output)
-    #Displaying the output to the user
     cv2.imshow("magic", final_output)
     cv2.waitKey(1)
-
-
-cap.release()
-out.release()
+    
+cam.release()
+#out.release()
 cv2.destroyAllWindows()
